b6798c282558fadc085ad436f950b11a10f6d075
James <james@nerdcademy.dev>
2021-07-29 21:42:56 -0400

    Introduction to variables in Go code

---
+++ 02_variables/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func main() {
+	// var firstName string = "James"
+	// fmt.Println(firstName)
+
+	// var firstName = "James"
+	// var lastName string
+
+	// fmt.Println(firstName)
+	// fmt.Println(lastName)
+
+	// var firstName, lastName string
+	// var i, j, k int
+
+	// var b, f, s = true, 2.3, "string"
+
+	// firstName := "James"
+	// fmt.Println(firstName)
+
+	// var i, j := 0, 1
+
+	const PI = 3.14159
+
+	fmt.Println(PI)
+
+	// ...
+
+	// PI = 1.0 // does not work!
+
+	// fmt.Println(PI)
+}
